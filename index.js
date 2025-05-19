4f5234dfd0e064d34f0479975f12c6640c266def
Aleksei TSuranov <unbut.toned@yahoo.com>
2025-05-20 00:45:30 +0800

    2

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const db = mysql.createPool({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    port: 3306,
+    port: process.env.PORT,
     waitForConnections: true,
     connectionLimit: 10,
     connectTimeout: 10000, // 10 секунд таймаут
